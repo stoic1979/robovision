a58ef3b3e6cac4234d3e685a38d2d34200cdc083
Navjot Singh <weavebytes@gmail.com>
2018-06-13 14:14:25 +0530

    cli to convert img to grayscale

--- opencv.py
+++ opencv.py
@@ -4,15 +4,16 @@
 
 import argparse
 import sys
+import cv2
 
 
 class Opencv(object):
 
     def __init__(self):
 
-        parser = argparse.ArgumentParser( description='Opencv commandline')
+        parser = argparse.ArgumentParser(description='Opencv commandline')
         parser.add_argument('command', help='Subcommand to run')
-        
+
         args = parser.parse_args(sys.argv[1:2])
         if not hasattr(self, args.command):
             print("Unrecognized command")
@@ -23,22 +24,32 @@ class Opencv(object):
         getattr(self, args.command)()
 
     def to_grayscale(self):
-        print ("convering to grayscale")
+        try:
 
-        parser = argparse.ArgumentParser(description='Convert an image to grayscale')
+            parser = argparse.ArgumentParser(
+                    description='Convert an image to grayscale')
 
-        # required fields for creating a gitlab user
-        parser.add_argument('--input', help='input image name/path')
-        parser.add_argument('--output', help='output image name/path')
+            # required fields for creating a gitlab user
+            parser.add_argument('--input', help='input image name/path')
+            parser.add_argument('--output', help='output image name/path')
 
-        # parse args for this command
-        args = parser.parse_args(sys.argv[2:])
+            # parse args for this command
+            args = parser.parse_args(sys.argv[2:])
 
-        print("Converted '%s' to grayscale and saved in '%s'" % (args.input, args.output) )
+            # converting image to grayscale
+            img = cv2.imread(args.input, 0)
 
+            # saving grayscaled image to output file
+            cv2.imwrite(args.output, img)
+
+            print(
+                "Converted '%s' to grayscale and saved in '%s'" %
+                (args.input, args.output))
+        except Exception as exp:
+            print(
+                "Got exception while converting image to grayscale: ",
+                str(exp))
 
 
 if __name__ == "__main__":
     Opencv()
-
-
