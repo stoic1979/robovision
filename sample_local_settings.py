149a5067b4b2f82de2c124ddd4d7e18eefd917ca
Navjot Singh <weavebytes@gmail.com>
2018-06-17 21:32:54 +0530

    adding face trainer

---
+++ sample_local_settings.py
@@ -0,0 +1,10 @@
+#
+# This is a sample file for local settings
+# rename it to local_settings.py
+#
+# PLEASE DO NOT commit your local_settings.py to version control !!!
+#
+# local settings, configs, paths for datasets etc.
+#
+
+FACE_IMAGES_DATASET_DIR = "some absolute local path on your computer"
---
+++ trainer_face.py
@@ -0,0 +1,69 @@
+#
+# a trainer for faces, inorder to identify faces with names/labels
+#
+# reference
+# https://www.youtube.com/watch?v=PmZ29Vta7Vc
+#
+
+import os
+import numpy as np
+from PIL import Image
+import cv2 as cv
+import pickle
+
+from local_settings import FACE_IMAGES_DATASET_DIR
+
+face_cascade = cv.CascadeClassifier('./cascades/haarcascades_cuda/haarcascade_frontalface_default.xml')
+recognizer = cv.face.LBPHFaceRecognizer_create() 
+
+y_labels = []
+x_train = []
+cur_id = 0
+label_ids = {}
+
+
+# fetching images from dataset for training
+for root, dirs, files in os.walk(FACE_IMAGES_DATASET_DIR):
+    for file in files:
+        if file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".png"):
+            full_path = os.path.join(root, file)
+            label = os.path.basename(root).replace(" ", "-").lower()
+
+            if not label in label_ids:
+                label_ids[label] = cur_id
+                cur_id += 1
+
+            img_id = label_ids[label]
+            print (label, img_id, full_path)
+
+            #y_labels.append(label)
+            #x_train.append(full_path)
+
+            # convert image to grayscale
+            pil_image = Image.open(full_path).convert("L")
+
+            # convery grayscale image to numpy array
+            image_array = np.array(pil_image, "uint8")
+            #print(image_array)
+
+            faces = face_cascade.detectMultiScale(image_array, 1.3, 5)
+
+
+            for (x,y,w,h) in faces:
+                # define roi for eyes detection,ideally, we should detect
+                # eyes within the rectangular bounds of a face
+                roi = image_array[y:y+h, x:x+w]
+                x_train.append(roi)
+                y_labels.append(img_id)
+
+#print (y_labels)
+#print (x_train)
+print (label_ids)
+
+# save trained labels
+with open("labels.pickle", 'wb') as f:
+    pickle.dump(label_ids)
+
+
+recognizer.train(x_train, np.array(y_labels))
+recognizer.save("trainer.yml")
