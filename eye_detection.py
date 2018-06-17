babc2078bb752aa8690ab3ea210d71e31a0fd515
Navjot Singh <weavebytes@gmail.com>
2018-06-17 18:48:04 +0530

    process img func to detect/highlight eye

--- eye_detection.py
+++ eye_detection.py
@@ -5,6 +5,7 @@
 import numpy as np
 import cv2 as cv
 
+
 class EyeDetector:
 
     def __init__(self, face_cascade_xml, eye_cascade_xml):
@@ -12,10 +13,10 @@ class EyeDetector:
         # lets us pretrained cascade classifiers of opencv for face and eyes detection
 
         # train/initialize face classifier
-        face_cascade = cv.CascadeClassifier(face_cascade_xml)
+        self.face_cascade = cv.CascadeClassifier(face_cascade_xml)
 
         # train/initialize eye classifier
-        eye_cascade = cv.CascadeClassifier(eye_cascade_xml)
+        self.eye_cascade = cv.CascadeClassifier(eye_cascade_xml)
 
         # reading data from webcam
         # for internal webcam on laptop use 0
@@ -26,25 +27,7 @@ class EyeDetector:
 
             ret, img = cap.read()
 
-            # HAAR cascade will need a grayscaled image to detect faces and eyes
-            gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-
-            # lets find faces in the image and get their positions
-            faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-            for (x,y,w,h) in faces:
-
-                # define roi for eyes detection,ideally, we should detect
-                # eyes within the rectangular bounds of a face
-                roi_gray = gray[y:y+h, x:x+w]
-                roi_color = img[y:y+h, x:x+w]
-
-                # drawing rects for eyes
-                eyes = eye_cascade.detectMultiScale(roi_gray)
-                for (ex,ey,ew,eh) in eyes:
-                    cv.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (255, 0, 0), 2)
-
-            # showing image
-            cv.imshow('Haar Face Detection', img)
+            self.process_image(img)
 
             # wait for key for 10 mscs, or continue
             k = cv.waitKey(10) & 0xff
@@ -55,6 +38,31 @@ class EyeDetector:
 
         cap.release()
         cv.destroyAllWindows()
+
+    def process_image(self, img):
+        """
+        this function detects eyes in a given image,
+        and draws an outline rectangle across eyes
+        """
+        # HAAR cascade will need a grayscaled image to detect faces and eyes
+        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+        # lets find faces in the image and get their positions
+        faces = self.face_cascade.detectMultiScale(gray, 1.3, 5)
+        for (x,y,w,h) in faces:
+
+            # define roi for eyes detection,ideally, we should detect
+            # eyes within the rectangular bounds of a face
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = img[y:y+h, x:x+w]
+
+            # drawing rects for eyes
+            eyes = self.eye_cascade.detectMultiScale(roi_gray)
+            for (ex,ey,ew,eh) in eyes:
+                cv.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (255, 0, 0), 2)
+
+        # showing image
+        cv.imshow('Haar Face Detection', img)
 
 if __name__ == "__main__":
     # path to Haar face classfier's xml file
