0b2e6831c93bdfcef6b14a06b8b95bcd39cf0523
Navjot Singh <weavebytes@gmail.com>
2018-06-22 22:48:31 +0530

    generated dataset dir

--- trainer_face.py
+++ trainer_face.py
@@ -13,56 +13,66 @@ import pickle
 
 from local_settings import FACE_IMAGES_DATASET_DIR
 
-face_cascade = cv.CascadeClassifier('./cascades/haarcascades_cuda/haarcascade_frontalface_default.xml')
-recognizer = cv.face.LBPHFaceRecognizer_create() 
+class FaceTrainer:
 
-y_labels = []
-x_train = []
-cur_id = 0
-label_ids = {}
+    def __init__(self, face_cascade_xml):
 
-# fetching images from dataset for training
-for root, dirs, files in os.walk(FACE_IMAGES_DATASET_DIR):
-    for file in files:
-        if file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".png"):
-            full_path = os.path.join(root, file)
-            label = os.path.basename(root).replace(" ", "-").lower()
+        face_cascade = cv.CascadeClassifier('./cascades/haarcascades_cuda/haarcascade_frontalface_default.xml')
+        recognizer = cv.face.LBPHFaceRecognizer_create() 
 
-            if not label in label_ids:
-                label_ids[label] = cur_id
-                cur_id += 1
+        y_labels = []
+        x_train = []
+        cur_id = 0
+        label_ids = {}
 
-            img_id = label_ids[label]
-            print (label, img_id, full_path)
+        # fetching images from dataset for training
+        for root, dirs, files in os.walk(FACE_IMAGES_DATASET_DIR):
+            for file in files:
+                if file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".png"):
+                    full_path = os.path.join(root, file)
+                    label = os.path.basename(root).replace(" ", "-").lower()
 
-            #y_labels.append(label)
-            #x_train.append(full_path)
+                    if not label in label_ids:
+                        label_ids[label] = cur_id
+                        cur_id += 1
 
-            # convert image to grayscale
-            pil_image = Image.open(full_path).convert("L")
+                    img_id = label_ids[label]
+                    print (label, img_id, full_path)
 
-            # convery grayscale image to numpy array
-            image_array = np.array(pil_image, "uint8")
-            #print(image_array)
+                    #y_labels.append(label)
+                    #x_train.append(full_path)
 
-            faces = face_cascade.detectMultiScale(image_array, 1.3, 5)
+                    # convert image to grayscale
+                    pil_image = Image.open(full_path).convert("L")
 
+                    # convery grayscale image to numpy array
+                    image_array = np.array(pil_image, "uint8")
+                    #print(image_array)
 
-            for (x,y,w,h) in faces:
-                # define roi for eyes detection,ideally, we should detect
-                # eyes within the rectangular bounds of a face
-                roi = image_array[y:y+h, x:x+w]
-                x_train.append(roi)
-                y_labels.append(img_id)
-
-#print (y_labels)
-#print (x_train)
-print (label_ids)
-
-# save trained labels
-with open("labels.pickle", 'wb') as f:
-    pickle.dump(label_ids, f)
+                    faces = face_cascade.detectMultiScale(image_array, 1.3, 5)
 
 
-recognizer.train(x_train, np.array(y_labels))
-recognizer.save("trainer.yml")
+                    for (x,y,w,h) in faces:
+                        # define roi for eyes detection,ideally, we should detect
+                        # eyes within the rectangular bounds of a face
+                        roi = image_array[y:y+h, x:x+w]
+                        x_train.append(roi)
+                        y_labels.append(img_id)
+
+        #print (y_labels)
+        #print (x_train)
+        print (label_ids)
+
+        # save trained labels
+        with open("labels.pickle", 'wb') as f:
+            pickle.dump(label_ids, f)
+
+
+        recognizer.train(x_train, np.array(y_labels))
+        recognizer.save("trainer.yml")
+
+if __name__ == "__main__":
+    # path to Haar face classfier's xml file
+    face_cascade_xml = './cascades/haarcascades_cuda/haarcascade_frontalface_default.xml'
+
+    ft = FaceTrainer(face_cascade_xml)
