0bd7b0cb26bdcaef8d7b1673c0b240fc7c3dfd3c
Navjot Singh <weavebytes@gmail.com>
2018-06-24 18:10:30 +0530

    added (c) header

--- app.py
+++ app.py
@@ -1,3 +1,26 @@
+#!/usr/bin/python3
+
+"""
+ RoboVision
+ ______________
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ Project Author/Architect: Navjot Singh <weavebytes@gmail.com>
+
+"""
+
 import sys
 import os
 
@@ -5,6 +28,9 @@ from PyQt5 import QtGui, QtCore, uic
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QMessageBox, QListWidgetItem
 from PyQt5.uic import loadUi
+
+from logger import get_logger
+log = get_logger()
 
 DIRPATH = os.path.join(os.path.dirname(os.path.abspath(__file__)))
 
@@ -25,7 +51,7 @@ class AppWindow(QMainWindow):
         self.btnOk.clicked.connect(self.ok_pressed)
 
     def ok_pressed(self):
-        print ("[AppWindow] :: ok")
+        log.debug("[AppWindow] :: ok")
         self.show_msgbox("AppWindow", "Its ok")
 
     def show_msgbox(self, title, text):
